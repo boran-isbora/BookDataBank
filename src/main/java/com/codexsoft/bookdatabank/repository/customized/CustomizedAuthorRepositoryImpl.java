96d089f3ae1f4eaa9e039bb5eaa570a058662673
boranisbora <boran.isbora@outlook.com>
2023-12-27 14:11:04 +0300

    refactor repository transaction

--- src/main/java/com/codexsoft/bookdatabank/repository/customized/CustomizedAuthorRepositoryImpl.java
+++ src/main/java/com/codexsoft/bookdatabank/repository/customized/CustomizedAuthorRepositoryImpl.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityManager;
 
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
